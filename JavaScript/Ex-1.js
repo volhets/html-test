2c2fa06464be2aba9874ece55556147230310ecd
Volodymyr Hets <volhets@gmail.com>
2019-02-25 17:35:32 +0200

    Ex-1 JavaScript

---
+++ JavaScript/Ex-1.js
@@ -0,0 +1,7 @@
+//малюємо стільки котів, скільки захочемо!
+var drawCats = function (howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + "=^.^=");
+    }
+};
+drawCats(10); // Замість 10 можете вказати будь-яке число, яке вам заманеться.
